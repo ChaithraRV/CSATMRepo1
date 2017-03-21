c1b818b6dcc9a05c8d2f10bcb30980a3f9e2fb8a
Chaithra <chaithrarv.reddy@in.ibm.com>
2017-03-21 14:15:26 +0530

    initial check in
    Signed-off-by: Chaithra <chaithrarv.reddy@in.ibm.com>

---
+++ TestGIT2/src/Pack2/Testfile2.java
@@ -0,0 +1,12 @@
+package Pack2;
+
+import org.testng.annotations.Test;
+
+public class Testfile2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Test create file");
+	  System.out.println("first commit");
+  }
+}
