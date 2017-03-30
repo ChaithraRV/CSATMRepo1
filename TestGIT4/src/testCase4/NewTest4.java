697652d83e560a4b849d916b200a292cf9a99b19
Chaithra <chaithrarv.reddy@in.ibm.com>
2017-03-30 13:57:24 +0530

    Commit1

---
+++ TestGIT4/src/testCase4/NewTest4.java
@@ -0,0 +1,13 @@
+package testCase4;
+
+import org.testng.annotations.Test;
+
+public class NewTest4 {
+  @Test
+  public void f() {
+	  
+	  
+	  System.out.println("Test initial");
+	  System.out.println("Test commit1");
+  }
+}
